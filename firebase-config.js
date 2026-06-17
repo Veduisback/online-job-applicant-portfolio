f79ef5bfae19bcdcab8ad188788fa900efe75d00
Vedang Jaiswal <veduisback@gmail.com>
2026-06-18 03:03:50 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -9,5 +9,6 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
-const auth = firebase.auth();
-const db = firebase.firestore();
+window.auth = firebase.auth();
+
+window.db = firebase.firestore();
