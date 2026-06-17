0c2716182cce046e2eca2875b7ad801c4bd017e5
Vedang Jaiswal <veduisback@gmail.com>
2026-06-18 03:43:21 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -18,8 +18,9 @@ app.use(express.json());
 
 // FIREBASE ADMIN
 
-const serviceAccount =
-require("./serviceAccountKey.json");
+const serviceAccount = JSON.parse(
+process.env.FIREBASE_SERVICE_ACCOUNT
+);
 
 
 admin.initializeApp({
